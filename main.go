a3ba486aa7489074adfe3bd588df6d632788850c
shubham_dixit <shubam.dixit@janio.asia>
2026-01-05 16:46:25 +0530

    Primitive datatypes

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
 func main() {
+	//var c bool
+	//var c string // uniocde (utf-8),asciii
+
+	/**
+	int  int8  int16  int32  int64  // it can store both positivce and negative
+	uint uint8 uint16 uint32 uint64 uintptr  // it can only store insiogned values that is positive values
+	*/
+
+	//var z int32
+
+	//var z uint8 // it can store an integer that can be respreented in 8 bits
+	//z = -127    // the maximum value that it cna store is from -127 to 127
+	//fmt.Println(z)
+	// uintptr is for storing pointer variables
+	//var d uint8
+	//d = 9
+	//var c uintptr
+	//c = d
+
+	// byte // 1 byte =8 bits
+	//
+	//var c byte  // []byte slice
+	//
+	//c = 9
+	// rune  // alias for int32
+	//var c rune
+	//c = 90
+
+	//float32 float64
+
+	//var t float32
+	//t = 8999999999999999999999999999999999999999.900000000000000000000000000
+
+	// complex64 complex128 // 2+3i (-1)
+	//var z1 complex128 = 3 + 4i
 
 }
